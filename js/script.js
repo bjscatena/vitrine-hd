37182b8450b300237cd0a01de1101d936d51bad6
bjscatena <brunojsxc@gmail.com>
2019-11-10 12:21:48 -0300

    adicionada sessao de tecnologias

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ for (const menuLink of overlayMenuLinks) {
 const nav = document.querySelector('.nav');
 
 // Get the offset position of the navbar
-var sticky = nav.offsetTop;
+const sticky = nav.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNavigation() {
